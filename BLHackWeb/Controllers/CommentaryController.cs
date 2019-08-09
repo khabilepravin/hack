4315efd84f5fac6982d7ef3438a9b90d062c8f4e
Pravin Khabile <khabilepravin@gmail.com>
2019-08-09 13:45:14 +1000

    Refactoring to use .net 2.1 and removed unnecessary projects

---
+++ BLHackWeb/Controllers/CommentaryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+
+namespace ByLawyersHackWeb.Controllers
+{
+    public class CommentaryController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public CommentaryController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        public IActionResult TableOfContents()
+        {
+            var provider = new PhysicalFileProvider(_hostingEnvironment.ContentRootPath);
+           
+            var fileInfo = provider.GetFileInfo("wwwroot/FULL COMMENTARY - PERSONAL INJURY (NSW).pdf");
+
+            var stream = fileInfo.CreateReadStream();
+
+            var contents =  PdfLib.PdfIndexParser.GetContentPagesText(stream, 2);
+
+           var tableOfContents = PdfLib.PdfIndexParser.GetTableOfContents(contents);
+
+            return View(tableOfContents);
+        }
+    }
+}
